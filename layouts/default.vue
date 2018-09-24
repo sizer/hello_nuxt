1532d125d59dda51c68944edbf0a3be3115ee60b
sizer <shinse.tanaka@gmail.com>
2018-09-24 18:57:33 +0900

    write layout.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
+    <header>
+      <h1>MEGAHAGE!!</h1>
+      <nav>
+        <nuxt-link to="/contents">contents</nuxt-link>
+        <nuxt-link to="/profile">profile</nuxt-link>
+      </nav>
+    </header>
     <nuxt/>
   </div>
 </template>
 
 <style>
-html
-{
+html {
   font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
@@ -16,38 +22,16 @@ html
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
-*, *:before, *:after
-{
-  box-sizing: border-box;
+body {
   margin: 0;
 }
-.button--green
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+header {
+  background-color: black;
+  color: white;
 }
-.button--green:hover
-{
-  color: #fff;
-  background-color: #3b8070;
-}
-.button--grey
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-.button--grey:hover
-{
-  color: #fff;
-  background-color: #35495e;
+
+header h1 {
+  margin: 0;
+  padding: 0.5em;
 }
 </style>
